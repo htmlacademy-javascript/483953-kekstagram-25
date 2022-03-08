3f25353b4c1443e743f15e8a1cd408d8235cb6dd
Zharova Anna <zharova@restream.rt.ru>
2022-03-08 11:56:49 +0100

    Генерация данных

--- js/main.js
+++ js/main.js
@@ -17,12 +17,100 @@ function getRandomNumber(firstNumber, secondNumber) {
   return getRandomIntInclusive(firstNumber, secondNumber);
 }
 
-getRandomNumber(-2, 5.5);
-
 // имя_функции(проверяемая_строка, максимальная_длина); // Результат: true, если строка проходит по длине, и false — если не проходит
 
 function compareStringLenth(string, maxLength) {
   return string.length < maxLength;
 }
 
-compareStringLenth('Comment', 20);
+const altText = [
+  'cat',
+  'cat & food',
+  'cat & dog',
+  'cats',
+  'white cat',
+  'tabby cat',
+  'red cat',
+  'black cat',
+  'gray cat',
+  'cat & cat',
+  'white cat & food',
+  'tabby cat & food',
+  'red cat & food',
+  'black cat & food',
+  'gray cat & food',
+  'white cat & dog',
+  'tabby cat & dog',
+  'red cat & dog',
+  'black cat & dog',
+  'gray cat & dog',
+  'white cat & toy',
+  'tabby cat & toy',
+  'red cat & toy',
+  'black cat & toy',
+  'gray cat & toy'
+];
+
+const message = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const nickname = [
+  'Пол',
+  'Полина',
+  'Олег',
+  'Ольга',
+  'Виталий',
+  'Виталина',
+  'Майкл',
+  'Мишель',
+  'Флориан',
+  'Флора'
+];
+
+const createMessage = function (){
+  let text = '';
+  const k = getRandomNumber(1, 2);
+  for (let i = 1; i <= k; i++){
+    text += ' ' + message[getRandomNumber(0, message.length - 1)];
+  }
+  return text.slice(1);
+};
+
+let idComment = 0;
+
+const makeComment = function (){
+  idComment = idComment + getRandomNumber(1, 10);
+  const comment = {
+    id: idComment,
+    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+    message: createMessage(),
+    name: nickname[getRandomNumber(0, nickname.length - 1)]
+  };
+  return comment;
+};
+
+const createDescription = function (id){
+  const url = `photos/${id}.jpg`;
+  const description = {
+    id: id,
+    url: url,
+    description: altText[id - 1],
+    likes: getRandomNumber(15, 200),
+    comments: makeComment()
+  };
+  return description;
+};
+
+const makePhoto = function (){
+  const photos = [];
+  for (let i = 0; i < 25; i++){
+    photos[i] = createDescription(i + 1);
+  }
+  return photos;
+};
