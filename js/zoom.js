cf00def2a20dc3bd8124b32ed096aae798641065
Zharova Anna <zharova@restream.rt.ru>
2022-04-09 17:33:12 +0200

    Загрузка комментариев

--- js/zoom.js
+++ js/zoom.js
@@ -7,13 +7,15 @@ const photoImg = document.querySelector('.big-picture__img img');
 const photoBig = document.querySelector('.big-picture');
 const photoLikes = document.querySelector('.likes-count');
 const photoCommentsCount = document.querySelector('.comments-count');
-const photoCommentsCountContainer = document.querySelector('.social__comment-count');
 const photoCommentsLoader = document.querySelector('.comments-loader');
 const photoDescription = document.querySelector('.social__caption');
 const commentsContainer = document.querySelector('.social__comments');
 const fragment = document.createDocumentFragment();
 const photoCommentsItem = document.querySelector('.social__comment');
 const closeBtn = document.querySelector('.big-picture__cancel');
+const NEXT_COMMENTS_COUNT = 5;
+let index;
+let elem;
 
 const onPopupEscKeydown = (evt) => {
   if (evt.key === 'Escape') {
@@ -30,27 +32,26 @@ function showBigPhoto (evt) {
     document.addEventListener('keydown', onPopupEscKeydown);
 
     const RE = /([0-9]+)\.jpg/;
-    const i = evt.target.src.match(RE)[1] - 1;
+    elem = evt.target.src.match(RE)[1] - 1;
 
     // Адрес изображения url подставьте как src изображения внутри блока .big-picture__img.
-    photoImg.src = photos[i].url;
+    photoImg.src = photos[elem].url;
 
     // Количество лайков likes подставьте как текстовое содержание элемента .likes-count.
-    photoLikes.textContent = photos[i].likes;
+    photoLikes.textContent = photos[elem].likes;
 
     // Количество комментариев comments подставьте как текстовое содержание элемента .comments-count.
-    photoCommentsCount.textContent = photos[i].comments.length;
+    photoCommentsCount.textContent = photos[elem].comments.length;
 
     // Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments. Разметка каждого комментария должна выглядеть так:
 
-    setComments(photos[i]);
+    commentsContainer.innerHTML = ''; // сломается, если будет 0 комментариев!
+    index = 0;
+    setComments(photos[elem], 0);
 
     // Описание фотографии description вставьте строкой в блок .social__caption.
-    photoDescription.textContent = photos[i].description;
+    photoDescription.textContent = photos[elem].description;
   }
-  // После открытия окна спрячьте блоки счётчика комментариев .social__comment-count и загрузки новых комментариев .comments-loader, добавив им класс hidden, с ними мы разберёмся позже, в другом домашнем задании.
-  photoCommentsCountContainer.classList.add('hidden');
-  photoCommentsLoader.classList.add('hidden');
 
   // После открытия окна добавьте тегу <body> класс modal-open, чтобы контейнер с фотографиями позади не прокручивался при скролле. При закрытии окна не забудьте удалить этот класс.
   document.body.classList.add('modal-open');
@@ -58,17 +59,19 @@ function showBigPhoto (evt) {
 
 pictureList.addEventListener('click', showBigPhoto);
 
-  // <li class="social__comment">
-  //     <img
-  //         class="social__picture"
-  //         src="{{аватар}}"
-  //         alt="{{имя комментатора}}"
-  //         width="35" height="35">
-  //     <p class="social__text">{{текст комментария}}</p>
-  // </li>
+// <li class="social__comment">
+//     <img
+//         class="social__picture"
+//         src="{{аватар}}"
+//         alt="{{имя комментатора}}"
+//         width="35" height="35">
+//     <p class="social__text">{{текст комментария}}</p>
+// </li>
 
-function setComments (photo) {
-  for (let i = 0; i < photo.comments.length; i++){
+// В модуле, который отвечает за отрисовку окна с полноразмерным изображением, доработайте код по выводу списка комментариев таким образом, чтобы список показывался не полностью, а по 5 элементов, и следующие 5 элементов добавлялись бы по нажатию на кнопку «Загрузить ещё». Не забудьте реализовать обновление числа показанных комментариев в блоке .social__comment-count.
+
+function setComments (photo, commentsPage) {
+  for (let i = commentsPage * NEXT_COMMENTS_COUNT; i < NEXT_COMMENTS_COUNT * (commentsPage + 1); i++){
     const chunk = photoCommentsItem.cloneNode(true);
     const photoComment = chunk.querySelector('.social__comment img');
     photoComment.src = photo.comments[i].avatar;
@@ -77,9 +80,13 @@ function setComments (photo) {
     photoCommentText.textContent = photo.comments[i].message;
     fragment.appendChild(chunk);
   }
-  commentsContainer.innerHTML = ''; // сломается, если будет 0 комментариев!
   commentsContainer.appendChild(fragment);
 }
+
+photoCommentsLoader.addEventListener ('click', () => {
+  index++;
+  setComments(photos[elem], index);
+});
 
 // Напишите код для закрытия окна по нажатию клавиши Esc и клике по иконке закрытия.
 
